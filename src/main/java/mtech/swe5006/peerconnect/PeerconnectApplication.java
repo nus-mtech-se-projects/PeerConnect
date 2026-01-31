f0a29669679017f1e394678282cb2b7d1bac8471
sonepyie <sonepyie@gmail.com>
2026-01-31 10:12:36 +0800

    Initial Commit.

---
+++ src/main/java/mtech/swe5006/peerconnect/PeerconnectApplication.java
@@ -0,0 +1,13 @@
+package mtech.swe5006.peerconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtech/swe5006/peerconnect/PeerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package mtech.swe5006.peerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
